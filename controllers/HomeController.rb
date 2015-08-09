e1bc0882c5a515415e3ec67ae4ce005626931c28
Dylan <invision33@gmail.com>
2015-08-08 23:37:27 -0500

    add db building route

--- controllers/HomeController.rb
+++ controllers/HomeController.rb
@@ -1,13 +1,17 @@
 class HomeController < ApplicationController 
 get '/' do 
-	
-	# room1 = Room.create(:name => 'spooky')
-	# room2 = Room.create(:name => 'vapor')
-	# Playlist.create(:title => room1.name, :link => 'spooky.com')
-	# Playlist.create(:title => room2.name, :link => 'vapor.wave')
-	# @playlists = Playlist.first
+	'go to /build first' 
 	@rooms = Room.all 
 	@playlists = Playlist.all 
 	erb :index 
 end 
+
+get '/build'
+	room1 = Room.create(:name => 'spooky')
+	room2 = Room.create(:name => 'vapor')
+	Playlist.create(:title => room1.name, :link => 'spooky.com')
+	Playlist.create(:title => room2.name, :link => 'vapor.wave')
+	@playlists = Playlist.first
+end 
+
 end 
